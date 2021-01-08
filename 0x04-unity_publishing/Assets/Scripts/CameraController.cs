92484a3d4b0f395430bb2b0f60a90bee72ba0a70
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-07 21:09:17 -0800

    initial commit

---
+++ 0x04-unity_publishing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 intial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intial = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + intial;
+    }
+}
---
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 750f;
+    public int health = 5;
+    public int resetTime = 3;
+    
+    public Text scoreText;
+    public Text healthText;
+    public Text winloseText;
+
+    private Rigidbody Rb3D;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb3D = GetComponent<Rigidbody>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        HealthCheck();
+
+        if (Input.GetButton("Cancel"))
+        {
+            SceneManager.LoadScene("menu");
+        }
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 roll = new Vector3(horizontal, 0, vertical);
+        roll = roll * speed * Time.deltaTime;
+        Rb3D.AddForce(roll);
+    }
+
+    private void HealthCheck()
+    {
+        if (health == 0)
+        {
+            Lose();
+            StartCoroutine(LoadScene(resetTime));
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "Pickup":
+                score++;
+                SetScoreText();
+                Destroy(col.gameObject);
+                break;
+            case "Trap":
+                health--;
+                SetHealthText();
+                break;
+            case "Goal":
+                Win();
+                StartCoroutine(LoadScene(resetTime));
+                break;
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = $"Health: {health}";
+    }
+
+    void Win()
+    {
+        GameObject parent = winloseText.transform.parent.gameObject;
+        Image parentImage = parent.GetComponent<Image>();
+
+        winloseText.text = "You Win!";
+        winloseText.color = Color.black;
+        parentImage.color = Color.green;
+        parent.SetActive(true);
+    }
+
+    void Lose()
+    {
+        GameObject parent = winloseText.transform.parent.gameObject;
+        Image parentImage = parent.GetComponent<Image>();
+
+        winloseText.text = "Game Over!";
+        winloseText.color = Color.white;
+        parentImage.color = Color.red;
+        parent.SetActive(true);
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        health = 5;
+        score = 0;
+        SceneManager.LoadScene("maze");
+    }
+}
