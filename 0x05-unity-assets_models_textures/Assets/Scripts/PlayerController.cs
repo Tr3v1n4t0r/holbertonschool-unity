c5a64695de927e92285ddac3f7ed40cb2392fd13
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-29 16:03:11 -0800

    basic movement added

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float jumpForce = 2f;
+    
+    private Rigidbody body;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * Horizontal + transform.forward * Vertical;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.AddForce(Vector3.up * Mathf.Sqrt(jumpForce * -2 * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        body.MovePosition(body.position + move * Speed * Time.deltaTime);
+    }
+}
